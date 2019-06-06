8f16fbe6e760025821102464423e0f0ebbdd8b0e
81509 <815090488@qq.com>
2019-06-06 11:49:20 +0800

    ss

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,34 @@
+/**
+ * Created by codedrinker on 2019/6/1.
+ */
+
+function post() {
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                $("#comment_section").hide();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=69e311bad26e3f579725&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
+        },
+        dataType: "json"
+    });
+}
