0820c0194f8e1128e078c9e3d8c98865375ac82f
81509 <815090488@qq.com>
2019-06-09 23:46:45 +0800

    添加发布功能

---
+++ src/main/java/com/example/demo/controller/PublishController.java
@@ -0,0 +1,79 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.QuestionMapper;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Question;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+    @Autowired
+    QuestionMapper questionMapper;
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String dopublish(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Long id,
+            HttpServletRequest request,
+            Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        if (title == null || title.equals("")) {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登陆");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setTag(tag);
+        question.setDescription(description);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtMdified(question.getGmtMdified());
+        questionMapper.create(question);
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/model/Question.java
@@ -0,0 +1,94 @@
+package com.example.demo.model;
+
+public class Question {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtMdified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer commentCount;
+    private Integer LikeCount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtMdified() {
+        return gmtMdified;
+    }
+
+    public void setGmtMdified(Long gmtMdified) {
+        this.gmtMdified = gmtMdified;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getLikeCount() {
+        return LikeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        LikeCount = likeCount;
+    }
+}
